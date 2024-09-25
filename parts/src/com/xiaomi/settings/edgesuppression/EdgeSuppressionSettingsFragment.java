7313db12918e5dada3c8ec8c933f6af01cb7cb68
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-09-25 13:13:09 +0200

    sm8550-common: Migrate Edge Suppression Service to CompoundButton.OnCheckedChangeListener

--- parts/src/com/xiaomi/settings/edgesuppression/EdgeSuppressionSettingsFragment.java
+++ parts/src/com/xiaomi/settings/edgesuppression/EdgeSuppressionSettingsFragment.java
@@ -14,8 +14,8 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
 import android.widget.CompoundButton;
+import android.widget.FrameLayout;
 
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
