dd2dd6b1b6991e911d1af22463ee2a0ccac21273
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2024-11-11 23:04:50 +0100

    Build missing android.hidl.base@1.0 from source
    This is needed for WfdService to not crash.
    
    Fixes the error: dlopen failed: library "android.hidl.base@1.0.so" not found: needed by /system_ext/lib64/libwfdnative.so

--- common.mk
+++ common.mk
@@ -276,6 +276,7 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+		android.hidl.base@1.0 \
     android.hidl.allocator@1.0.vendor \
     android.hidl.memory.block@1.0.vendor \
     libhidltransport.vendor \
