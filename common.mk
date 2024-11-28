4c68c35d27e6fdda3609408c89dd14add71613c4
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2024-11-28 16:53:59 +0100

    build aidl variant of thermal

--- common.mk
+++ common.mk
@@ -591,9 +591,9 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-V1-ndk.vendor \
-    android.hardware.thermal@2.0.vendor \
-    android.hardware.thermal@2.0-service.qti-v2
+		android.hardware.thermal@2.0 \
+		android.hardware.thermal@2.0.vendor \
+    android.hardware.thermal-service.qti
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
