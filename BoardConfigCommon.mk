eba5055f29b3fa250ee48492df4e2bc6c379001d
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2024-11-28 02:40:23 +0100

    Migrate to common libqti-perfd-client and power-libperfmgr

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,6 +182,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 # SELinux
 include device/xiaomi/sepolicy/SEPolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
