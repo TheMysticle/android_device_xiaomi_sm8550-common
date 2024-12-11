6a4894c66dd34f40b86e9e27ad377ede2776ff74
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2024-12-11 13:48:22 +0100

    Remove Edge Rejection
    It never worked anyway

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.java
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.java
@@ -17,7 +17,6 @@ import android.view.Display;
 import android.view.Display.HdrCapabilities;
 
 import com.xiaomi.settings.display.ColorModeService;
-import com.xiaomi.settings.edgesuppression.EdgeSuppressionService;
 import com.xiaomi.settings.touch.TouchOrientationService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -44,8 +43,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
                 UserHandle.CURRENT);
 
         // Touchscreen
-        context.startServiceAsUser(new Intent(context, EdgeSuppressionService.class),
-                 UserHandle.CURRENT);
         context.startServiceAsUser(new Intent(context, TouchOrientationService.class),
                 UserHandle.CURRENT);
 
--- parts/src/com/xiaomi/settings/touch/TfWrapper.java
+++ parts/src/com/xiaomi/settings/touch/TfWrapper.java
@@ -8,7 +8,6 @@ package com.xiaomi.settings.touch;
 
 import android.os.IHwBinder.DeathRecipient;
 import android.util.Log;
-import java.util.ArrayList;
 
 import vendor.xiaomi.hw.touchfeature.V1_0.ITouchFeature;
 
@@ -45,11 +44,7 @@ public class TfWrapper {
         }
         if (DEBUG) Log.d(TAG, "setTouchFeatureParams: " + params);
         try {
-            if (params.valueList != null) {
-                touchfeature.setModeLongValue(0, params.mode, params.valueList.size(), params.valueList);
-              } else {
-                touchfeature.setModeValue(0, params.mode, params.value);
-              }
+            touchfeature.setModeValue(0, params.mode, params.value);
         } catch (Exception e) {
             Log.e(TAG, "setTouchFeatureParams failed!", e);
         }
@@ -58,26 +53,14 @@ public class TfWrapper {
     public static class TfParams {
         /* touchfeature parameters */
         final int mode, value;
-        final ArrayList<Integer> valueList;
 
         public TfParams(int mode, int value) {
             this.mode = mode;
             this.value = value;
-            this.valueList = null;
-        }
-
-        public TfParams(int mode, ArrayList<Integer> valueList) {
-            this.mode = mode;
-            this.value = 0;
-            this.valueList = valueList;
         }
 
         public String toString() {
-            if (valueList != null) {
-                return "TouchFeatureParams(" + mode + ", " + valueList + ")";
-              } else {
-                return "TouchFeatureParams(" + mode + ", " + value + ")";
-              }
+            return "TouchFeatureParams(" + mode + ", " + value + ")";
         }
     }
 }
