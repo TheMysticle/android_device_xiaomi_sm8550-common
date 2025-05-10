807aa5f2e9cd23ad9f4004bf8c892e2c1b9785f3
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-05-10 19:30:18 +0200

    sm8550-common: init: move init.qcom.rc & init.qcom.post_boot.sh to device
    Change-Id: Ia4c3446615f50132796811270a9af3c48227a90e

--- common.mk
+++ common.mk
@@ -190,6 +190,8 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.sm8550.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.sm8550.rc \
+    $(LOCAL_PATH)/init/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
+    $(LOCAL_PATH)/init/init.qcom.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.post_boot.sh \
     $(LOCAL_PATH)/init/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
 
 PRODUCT_PACKAGES += \
